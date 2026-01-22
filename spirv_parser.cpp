db94df043c934898982748b40aefed3584cf9cf2
Water Chika <water_chika@outlook.com>
2026-01-22 11:36:42 +0800

    add namespace spirv_parser and some types

--- spirv_parser.cpp
+++ spirv_parser.cpp
@@ -1,6 +1,12 @@
 #include "spirv_parser.hpp"
-
+#include <iostream>
 int main() {
-    open_spirv_file("test.spv");
+    try {
+        auto [file_mapping, spirv_code] = spirv_parser::open_spirv_file("test.spv");
+        std::cout << spirv_code.size() << std::endl;
+    }
+    catch (std::exception& e) {
+        std::cerr << e.what() << std::endl;
+    }
     return 0;
 }
--- spirv_parser.hpp
+++ spirv_parser.hpp
@@ -2,7 +2,32 @@
 
 #include <win32_helper.hpp>
 
-void open_spirv_file(std::filesystem::path path) {
+namespace spirv_parser {
+
+using word = uint32_t;
+using id = word;
+
+template<bool HasResult=false, bool HasInstructionType=false, uint32_t OperandsSize=0>
+struct instruction {
+    word word_count;
+    word opcode;
+    [[no_unique_address]]
+    cpp_helper::valid_if_t<HasResult, id> result;
+    [[no_unique_address]]
+    cpp_helper::valid_if_t<HasInstructionType, word> type;
+    [[no_unique_address]]
+    cpp_helper::valid_if_t<OperandsSize!=0, std::array<word,OperandsSize>> operands;
+};
+
+static_assert(
+    std::is_same_v<decltype(instruction<>{}.result), cpp_helper::empty_type> &&
+    std::is_same_v<decltype(instruction<true>{}.result), id>
+);
+
+auto open_spirv_file(std::filesystem::path path) {
     auto file_mapping = win32_helper::map_file(path);
     auto spirv_code = std::span{reinterpret_cast<const uint32_t*>(file_mapping.data()), file_mapping.size()/sizeof(uint32_t)};
+    return std::pair{std::move(file_mapping), spirv_code};
+}
+
 }
